75466c6fcfb3f59467622f75f3e57b371b63de24
taiba <adell_ll@yahoo.co.jp>
2025-12-19 23:43:36 +0900

    jQuery依存の削除と、GA4への移行

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,44 +1,44 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  var mapCanvas = document.getElementById('mapCanvas');
 
-  $(document).ready(function() {
+  // mapCanvasが存在しない場合は処理をスキップ
+  if (!mapCanvas) {
+    return;
+  }
 
-    var latlng = new google.maps.LatLng(36.265260 + 0.0015, 139.443936 - 0.001);
-    var myOptions = {
-      zoom: 16, /*拡大比率*/
-      center: latlng, /*表示枠内の中心点*/
-      mapTypeId: google.maps.MapTypeId.ROADMAP /*表示タイプの指定*/
-    };
-    var map = new google.maps.Map(document.getElementById('mapCanvas'), myOptions);
+  var latlng = new google.maps.LatLng(36.265260 + 0.0015, 139.443936 - 0.001);
+  var myOptions = {
+    zoom: 16, /*拡大比率*/
+    center: latlng, /*表示枠内の中心点*/
+    mapTypeId: google.maps.MapTypeId.ROADMAP /*表示タイプの指定*/
+  };
+  var map = new google.maps.Map(mapCanvas, myOptions);
 
-    var icon = new google.maps.MarkerImage('/assets/image/map_icon.png',
-      new google.maps.Size(128, 128), /*アイコンサイズ設定*/
-      new google.maps.Point(0, 0) /*アイコン位置設定*/
-    );
+  var icon = new google.maps.MarkerImage('/assets/image/map_icon.png',
+    new google.maps.Size(128, 128), /*アイコンサイズ設定*/
+    new google.maps.Point(0, 0) /*アイコン位置設定*/
+  );
 
-    latlng = new google.maps.LatLng(36.265260, 139.443936);
-    var markerOptions = {
-      position: latlng,
-      map: map,
-      icon: icon,
-    };
+  latlng = new google.maps.LatLng(36.265260, 139.443936);
+  var markerOptions = {
+    position: latlng,
+    map: map,
+    icon: icon,
+  };
 
-    var marker = new google.maps.Marker(markerOptions);
+  var marker = new google.maps.Marker(markerOptions);
 
-    var icon2 = new google.maps.MarkerImage('/assets/image/map_icon2.png',
-      new google.maps.Size(100, 100), /*アイコンサイズ設定*/
-      new google.maps.Point(0, 0) /*アイコン位置設定*/
-    );
+  var icon2 = new google.maps.MarkerImage('/assets/image/map_icon2.png',
+    new google.maps.Size(100, 100), /*アイコンサイズ設定*/
+    new google.maps.Point(0, 0) /*アイコン位置設定*/
+  );
 
-    var latlng2 = new google.maps.LatLng(36.267071, 139.440147);
-    var markerOptions2 = {
-      position: latlng2,
-      map: map,
-      icon: icon2,
-    };
+  var latlng2 = new google.maps.LatLng(36.267071, 139.440147);
+  var markerOptions2 = {
+    position: latlng2,
+    map: map,
+    icon: icon2,
+  };
 
-    var marker2 = new google.maps.Marker(markerOptions2);
-
-
-  });
-
+  var marker2 = new google.maps.Marker(markerOptions2);
 });
